be66b46838a564545b8a3a87a05d0557213cfcc4
Tibex88 <tibesolomon7@gmail.com>
2025-05-05 02:55:11 -0700

    fix: build css and js into the single file (html) cause galaxy wont serve assets

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-  base: '/',
+  plugins: [
+    react(),
+    viteSingleFile()
+  ],
+  base: '/interactivetool/ep/',
 })
