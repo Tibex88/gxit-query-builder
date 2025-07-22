b06f26a95d8c48b285ccd70953cc090adc19ec52
tibex88 <tibesolomon7@gmail.com>
2025-07-22 14:31:18 +0300

    feta: extract zip file and add to tables

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import { fileURLToPath } from 'url';
 import { argv } from 'process';
 import axios from 'axios';
 
+import AdmZip from 'adm-zip';
+const TEMP_ZIP_PATH = path.join('./', 'downloaded.zip'); // adjust path as needed
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -15,12 +19,18 @@ const PORT = process.env.PORT || 4173;
 const staticPath = path.join(__dirname, 'dist');
 const BASE_PREFIX = '/interactivetool/ep/:token/:job_id';
 
-const outputArgIndex = argv.indexOf('--output');
-let galaxyOutputPath = null;
+// const outputArgIndex = argv.indexOf('--output');
+const output1Index = argv.indexOf('--output');
+const output2Index = argv.indexOf('--output2');
 
+let galaxyOutputPaths = [null, null];
 
-if (outputArgIndex !== -1 && argv.length > outputArgIndex + 1) {
-  galaxyOutputPath = argv[outputArgIndex + 1];
+if (output1Index !== -1 && argv.length > output1Index + 1) {
+  galaxyOutputPaths[0] = argv[output1Index + 1];
+}
+
+if (output2Index !== -1 && argv.length > output2Index + 1) {
+  galaxyOutputPaths[1] = argv[output2Index + 1];
 }
 
 // Serve static files from React's dist folder
@@ -49,12 +59,32 @@ app.post(`${BASE_PREFIX}/save`, async (req, res) => {
         // add other headers here if needed
       }
     });
-    const fileStream = fs.createWriteStream(galaxyOutputPath);
+    // const fileStream = fs.createWriteStream(galaxyOutputPath);
+    const fileStream = fs.createWriteStream(TEMP_ZIP_PATH);
     await new Promise((resolve, reject) => {
     response.data.pipe(fileStream);
     response.data.on('error', reject);
     fileStream.on('finish', resolve);
   });
+
+    // Extract the ZIP
+  const zip = new AdmZip(TEMP_ZIP_PATH);
+  const zipEntries = zip.getEntries();
+  let fileIndex = 0;
+
+    for (const entry of zipEntries) {
+      console.log({entry: entry.entryName});
+    if (entry.entryName.endsWith('.tsv') && fileIndex < 2) {
+      const tsvContent = zip.readAsText(entry);
+      if (galaxyOutputPaths[fileIndex]) {
+        fs.writeFileSync(galaxyOutputPaths[fileIndex], tsvContent, 'utf8');
+        // console.log(`✅ Wrote ${entry.entryName} to ${galaxyOutputPaths[fileIndex]} - server.js:81`);
+      }
+      fileIndex++;
+    }
+  }
+
+  res.status(200).json({ message: 'TSV files extracted and logged' });
   } catch (err) {
     res.status(500).json({ message: 'Internal error', error: err.message });
   }
@@ -66,4 +96,4 @@ app.use((req, res) => {
   res.sendFile(path.join(staticPath, 'index.html'));
 });
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {}); 
