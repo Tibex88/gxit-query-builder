84fe9cdd7c971de7c796a6fa7f05e4b2fea0d184
tibex88 <tibesolomon7@gmail.com>
2025-09-02 14:52:44 +0300

    fix: navigate root path calls to annotation page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, redirect, RouterProvider, Navigate } from 'react-router-dom'
 import {QB} from './pages/query-builder.js'
 import { Result } from './pages/result.js'
 import { Param } from './pages/param.js'
@@ -16,6 +16,10 @@ import "./global.scss";
 let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc1MDA4MzE2MiwianRpIjoiZGM5YzlkZDctZGM2NC00MzBiLTgxMmYtYTYwOWEzZmVjNTZmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MTEsIm5iZiI6MTc1MDA4MzE2MiwiY3NyZiI6ImRiYTE4OGEzLTkzNGQtNGRmMC05ZWQzLTE2NDJkYzAyY2ZmZCIsImV4cCI6MTc1OTA4MzE2MiwidXNlcl9pZCI6MTEsImVtYWlsIjoidGliZXNvbG9tb243QGdtYWlsLmNvbSJ9.Sm50m91oV7HEkbEWMTJ2sxpYKL3ljBz2o3HAINCw8IQ"
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/interactivetool/ep/404kvqqida1i/ixw8gx33yn32" replace />,
+  },
   {
     path: "/interactivetool/ep/:id1/:id2",
     element: <QB />,
